03245e731e3e4a5673d8f13afed6a777d2204af2
mehfooz-analyst5 <analyst5@ugspk.com>
2023-12-22 14:58:57 +0500

    auth api view done

--- api_project/user/views.py
+++ api_project/user/views.py
@@ -3,8 +3,10 @@
 """
 
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework import generics, authentication, permissions
+from .serializers import UserSerializer, AuthTokenSerializer
+from rest_framework.settings import api_settings
+from rest_framework.authtoken.views import ObtainAuthToken
 
 # Create your views here.
 
@@ -12,3 +14,20 @@ from .serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth toekn for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retreive and return the authenticated user."""
+        return self.request.user
